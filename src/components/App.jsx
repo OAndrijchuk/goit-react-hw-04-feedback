fa8f000738e71483dae02b63dff9219042682791
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-08-07 17:09:25 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,62 @@
-import { Component } from "react"
-import { Section } from "./Section/Section"
-import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions"
-import { Statistics } from "./Statistics/Statistics"
-import { Notification } from "./Notification/Notification"
+import React, { useState } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  }
-  
-  onLeaveFeedback = ({ target }) => {
-    this.setState((preState) => ({[target.name]:preState[target.name]+1}))
-  }
-  countTotalFeedback = () => {
-    const {good, neutral, bad,}=this.state
-    return good + neutral + bad
-  }
-  countPositiveFeedbackPercentage = () => {
-    const rez = Math.round(100 / this.countTotalFeedback() * this.state.good)
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = ({ target }) => {
+    //  this.setState(preState => ({ [target.name]: preState[target.name] + 1 }));
+    switch (target.name) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+
+      default:
+        console.log('We have some problem :(');
+        break;
+    }
+  };
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    const rez = Math.round((100 / countTotalFeedback()) * good);
     if (Number.isNaN(rez)) {
       return 0;
     }
     return rez;
-}
+  };
 
-  render() {
-    return <>
-      <Section title='Please leave feedback'>
-        <FeedbackOptions onLeaveFeedback={this.onLeaveFeedback} buttons={this.state} />
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          onLeaveFeedback={onLeaveFeedback}
+          buttons={{ good, neutral, bad }}
+        />
       </Section>
-      <Section title='Statistics' >
-       { this.countTotalFeedback()
-            ? <Statistics {...this.state} total={this.countTotalFeedback} positivePercentage={this.countPositiveFeedbackPercentage} />
-            : <Notification message="There is no feedback"></Notification>}
-        
+      <Section title="Statistics">
+        {countTotalFeedback() ? (
+          <Statistics
+            {...{ good, neutral, bad }}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
       </Section>
     </>
-  }
-    
-
-}
+  );
+};
